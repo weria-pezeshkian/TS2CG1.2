a63e4a67ccef669493e6025636b75713a0bc0828
jan-stevens <stevens.jan.adriaan@gmail.com>
2024-07-08 18:42:09 +0200

    make project PyPi ready

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from pathlib import Path
@@ -6,7 +6,7 @@ import multiprocessing
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name):
+    def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
 
 class CMakeBuild(build_ext):
@@ -15,24 +15,27 @@ class CMakeBuild(build_ext):
             self.build_cmake(ext)
 
     def build_cmake(self, ext):
-        cwd = Path().absolute()
-        build_temp = Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
+        sourcedir = Path().absolute()
         extdir = Path(self.get_ext_fullpath(ext.name)).parent.absolute()
 
         cmake_args = [
             f'-DCMAKE_INSTALL_PREFIX={extdir}',
+            f'-DPYTHON_EXECUTABLE={sys.executable}',
+            '-DCMAKE_BUILD_TYPE=Release'
         ]
 
+        build_temp = Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+
         num_cores = multiprocessing.cpu_count()
-        build_args = ['--', f'-j{num_cores}']
+        build_args = ['--config', 'Release', '--', f'-j{num_cores}']
 
         os.chdir(build_temp)
-        self.spawn(['cmake', str(cwd)] + cmake_args)
+        self.spawn(['cmake', sourcedir] + cmake_args)
         if not self.dry_run:
             self.spawn(['cmake', '--build', '.'] + build_args)
             self.spawn(['cmake', '--install', '.'])
-        os.chdir(cwd)
+        os.chdir(sourcedir)
 
 setup(
     name='TS2CG',
@@ -42,7 +45,6 @@ setup(
     cmdclass={'build_ext': CMakeBuild},
     package_data={'TS2CG': ['SOL', 'PLM', 'PCG']},
     include_package_data=True,
-    install_requires=[],
     entry_points={
         'console_scripts': [
             'TS2CG=TS2CG.wrapper:main',
