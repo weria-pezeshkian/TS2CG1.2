80c030e341886164a2e19f97bdaaa8846793761b
jan-stevens <stevens.jan.adriaan@gmail.com>
2024-11-04 16:54:07 +0100

    initial commit of point class

---
+++ TS2CG/__init__.py
@@ -0,0 +1,10 @@
+"""
+TS2CG: converts triangulated surfaces to coarse-grained membrane models
+"""
+
+# Import the Python modules
+from .PointUpdaterClass.Point import Point
+
+__all__ = [
+    'Point',
+]
