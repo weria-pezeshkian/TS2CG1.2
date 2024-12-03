8a55ea7203ab1a4d7deb2b75b2ef8ff303885ccc
Fabian <fabian@schuhmaenner.net>
2024-12-03 16:53:27 +0100

    added scipy, matplotlib, and networkx as requirements

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     },
     include_package_data=True,
     python_requires='>=3.6',
-    install_requires=['numpy'],
+    install_requires=['numpy','scipy','networkx','matplotlib.pyplot'],
     entry_points={
         'console_scripts': [
             'TS2CG=TS2CG.run_modules:main',
